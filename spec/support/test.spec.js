8a6f16b6ce18418f1dc9c770dce800fec281424d
Marta Lenartovich <Marta_lenartovich@epam.com>
2021-06-18 19:59:54 +0300

    add some checks

--- spec/support/test.spec.js
+++ spec/support/test.spec.js
@@ -3,7 +3,7 @@ require("chromedriver");
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 40 * 1000;
 
 describe('search job test', function () {
-    it('should search Software Testing Engineer position in All Cities in Belarus', async function () {
+  it('should search Software Testing Engineer position in All Cities in Belarus', async function () {
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("https://www.epam.com/");
     await driver.manage().window().maximize();
@@ -13,22 +13,33 @@ describe('search job test', function () {
       "arguments[0].scrollIntoView();",
       jobSearchFilterForm
     );
-    //1. Enter 'Software Testing Engineer' in Keyword field
+    console.log('1. Enter "Software Testing Engineer" in Keyword field');
     const keyword = await driver.findElement(
       By.xpath('//input[@id="new_form_job_search_1445745853_copy-keyword"]')
     );
     await keyword.sendKeys("Software Testing Engineer");
-  
-    //2. Select 'All Cities in Belarus' option in Location dropdown
+
+    console.log(
+      '2. Select "All Cities in Belarus" option in Location dropdown'
+    );
     const citiesDropdownArrow = await driver.findElement(
       By.xpath('//span[@class="select2-selection__arrow"]')
     );
     await citiesDropdownArrow.click();
+    const chinaCheckbox = await driver.findElement(
+      By.xpath('//li[contains(text(), "China")]')
+    );
+    expect(chinaCheckbox).toBeDefined();
     //по непонятным причинам тут бывает выбирается брест, хотя поиск идет по тексту
-    const allCitiesDropdownCheckbox = await driver.findElement(By.xpath('//li[contains(text(), "All Cities in Belarus")]'));
-    await allCitiesDropdownCheckbox.click();
-  
-    //3. Select 'Software Test Engineering' skill in Skills dropdown
+    const allCitiesCheckbox = await driver.findElement(
+      By.xpath('//li[contains(text(), "All Cities in Belarus")]')
+    );
+    await driver.wait(() => allCitiesCheckbox.isDisplayed(), 5000);
+    await allCitiesCheckbox.click();
+
+    console.log(
+      '3. Select "Software Test Engineering" skill in Skills dropdown'
+    );
     await (await driver.findElement(By.className("selected-params"))).click();
     const skillsDropdown = await driver.findElement(
       By.css(".multi-select-dropdown:not(.hidden)")
@@ -39,19 +50,29 @@ describe('search job test', function () {
     );
     const softwareTestEngineeringCheckbox =
       softwareTestEngineeringCheckboxArray[1];
-    await driver.wait(until.elementIsEnabled(softwareTestEngineeringCheckbox),
-        10000);
+    await driver.wait(
+      until.elementIsEnabled(softwareTestEngineeringCheckbox),
+      10000
+    );
     await softwareTestEngineeringCheckbox.click();
-  
-    //4. Click 'Find' button
-    const findButton = await driver.findElement(By.xpath('//button[@type="submit"]'));
+    const labelSelected = await driver
+      .findElement(By.className("label"))
+      .getText();
+    const counter = await driver.findElement(By.className("counter")).getText();
+    expect(labelSelected + counter).toEqual("Selected:1");
+
+    console.log('4. Click "Find" button');
+    const findButton = await driver.findElement(
+      By.xpath('//button[@type="submit"]')
+    );
+    expect(await findButton.isEnabled()).toBeTrue();
     await findButton.click();
 
-    const resultLine = await driver.findElement(By.xpath('//h1[@class="search-result__heading"]'));
+    const resultLine = await driver.findElement(
+      By.xpath('//h1[@class="search-result__heading"]')
+    );
     expect(resultLine).toBeDefined();
 
-  
     await driver.quit();
-  
-    });
   });
+});
